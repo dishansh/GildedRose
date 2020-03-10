3f826c8e92dcfd2a221628d54a6fb55136ee092a
dishansh <dgsingh@deloitte.co.uk>
2020-03-10 15:43:28 +0000

    Added more tests, added max allowed quantity check

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -57,6 +57,7 @@ public class GildedRoseTest {
 	
 	@Test
 	public void qualityForAgedBrieIncreasesByDay() {
+		
 		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Aged Brie", 20, 32)));
 		inventoryApp.updateQuality();
 		inventoryApp.updateQuality();
@@ -77,5 +78,61 @@ public class GildedRoseTest {
 		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 20, 80)));
 		inventoryApp.updateQuality();
 		assertEquals(80, inventoryApp.getItems().get(0).getQuality());
+	}
+	
+	@Test
+	public void sellInDaysRemainConstantForSulfuras() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 20, 80)));
+		inventoryApp.updateQuality();
+		assertEquals(20, inventoryApp.getItems().get(0).getSellIn());
+	}
+	
+	@Test
+	public void standardDecreaseInQualityAfterSellByDateShouldBeTwiceAsNormal() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 0, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(20, inventoryApp.getItems().get(0).getQuality());
+	}
+	
+	@Test
+	public void backStagePassesQualityIncreaseAsSellByDateDecreases() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(23, inventoryApp.getItems().get(0).getQuality());	
+	}
+	
+	@Test
+	public void backStagePassesQualityIncreaseByTwo_whenSellByDateIsLessThanOrEqualToTen() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Backstage passes to a TAFKAL80ETC concert", 9, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(24, inventoryApp.getItems().get(0).getQuality());	
+	}
+	
+	@Test
+	public void backStagePassesQualityIncreaseByThree_whenSellByDateIsLessThanOrEqualToFive() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(25, inventoryApp.getItems().get(0).getQuality());	
+	}
+	
+	@Test
+	public void backStagePassesQualityDropsToZero_whenSellByDateIsPassed() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Backstage passes to a TAFKAL80ETC concert", -1, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(0, inventoryApp.getItems().get(0).getQuality());	
+	}
+	
+	@Test
+	public void conjuredItemQualityDecreasesTwiceThanNormal() {
+		
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Conjured Item", 6, 22)));
+		inventoryApp.updateQuality();
+		assertEquals(20, inventoryApp.getItems().get(0).getQuality());
 	}
 }
