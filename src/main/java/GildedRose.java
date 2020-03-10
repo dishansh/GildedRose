c5f3f56e6fe65bdb99dae7f133feff4ebdc7088d
dishansh <dgsingh@deloitte.co.uk>
2020-03-10 10:17:07 +0000

    Added more tests, Re-factored application class - GildedRose.java

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -1,20 +1,22 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class GildedRose {
 
 	private static List<Item> items = null;
 
-	public GildedRose() {
+	public GildedRose(List<Item> items) {
 		System.out.println("OMGHAI!");
 
-		items = new ArrayList<Item>();
-		items.add(new Item("+5 Dexterity Vest", 10, 20));
-		items.add(new Item("Aged Brie", 2, 0));
-		items.add(new Item("Elixir of the Mongoose", 5, 7));
-		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-		items.add(new Item("Conjured Mana Cake", 3, 6));
+		this.items = items;
+		
+		/*
+		 * items = new ArrayList<Item>(); items.add(new Item("+5 Dexterity Vest", 10,
+		 * 20)); items.add(new Item("Aged Brie", 2, 0)); items.add(new
+		 * Item("Elixir of the Mongoose", 5, 7)); items.add(new
+		 * Item("Sulfuras, Hand of Ragnaros", 0, 80)); items.add(new
+		 * Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)); items.add(new
+		 * Item("Conjured Mana Cake", 3, 6));
+		 */
 	}
 
 	public List<Item> getItems() {
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,18 +1,81 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
 
 import org.junit.Test;
-
 
 public class GildedRoseTest {
 
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void creationOfInventoryWithItem() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 15)));
+		assertEquals("Custom Item", inventoryApp.getItems().get(0).getName());
+	}
+
+	@Test
+	public void standardDecreaseInQualityAfterOneDay() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 15)));
+		inventoryApp.updateQuality();
+		assertEquals(14, inventoryApp.getItems().get(0).getQuality());
+	}
+
+	@Test
+	public void standardDecreaseInSellInAfterOneDay() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 15)));
+		inventoryApp.updateQuality();
+		assertEquals(19, inventoryApp.getItems().get(0).getSellIn());
+	}
+
+	@Test
+	public void standardDecreaseInQualityAfterTwoDays() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 15)));
+		inventoryApp.updateQuality();
+		inventoryApp.updateQuality();
+		assertEquals(13, inventoryApp.getItems().get(0).getQuality());
+	}
+
+	@Test
+	public void standardDecreaseInSellInAfterTwoDays() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 15)));
+		inventoryApp.updateQuality();
+		inventoryApp.updateQuality();
+		assertEquals(18, inventoryApp.getItems().get(0).getSellIn());
+	}
+
+	@Test
+	public void standardDecreaseInQualityShouldNotBeBelowZero() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Custom Item", 20, 0)));
+		inventoryApp.updateQuality();
+		assertEquals(0, inventoryApp.getItems().get(0).getQuality());
 	}
 	
 	@Test
-	public void testCreationOfItems() {	
-		GildedRose testItem = new GildedRose();
-		assertEquals(6, testItem.getItems().size());
+	public void qualityForAgedBrieIncreasesByDay() {
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Aged Brie", 20, 32)));
+		inventoryApp.updateQuality();
+		inventoryApp.updateQuality();
+		assertEquals(34, inventoryApp.getItems().get(0).getQuality());
+	}
+
+	@Test
+	public void qualityForAgedBrieCanNeverBeMoreThanFifty() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Aged Brie", 20, 50)));
+		inventoryApp.updateQuality();
+		assertEquals(50, inventoryApp.getItems().get(0).getQuality());
+	}
+
+	@Test
+	public void qualityOfSulfurusRemainsUnchanged() {
+
+		GildedRose inventoryApp = new GildedRose(Arrays.asList(new Item("Sulfuras, Hand of Ragnaros", 20, 80)));
+		inventoryApp.updateQuality();
+		assertEquals(80, inventoryApp.getItems().get(0).getQuality());
 	}
 }
